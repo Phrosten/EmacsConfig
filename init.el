83488458e6a539798019f2a1ad25eae5cb064f32
Jonas Heinrich <jonas.heinrich.business@gmail.com>
2018-07-24 04:09:03 -0400

    Upgraded emacs version, included packages for general development, clojure development.

--- init.el
+++ init.el
@@ -1,2 +1,23 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (org-babel-load-file "~/.emacs.d/config.org")
 (put 'upcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (cider clojure-mode py-autopep8 flycheck elpy magit projectile ox-gfm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
